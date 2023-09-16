484efafe087d1e4936f1c546eacf555496500e26
mustafaozx <mustafaozx@icloud.com>
2023-09-16 18:25:21 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { BarcodeFormat, ZXing } from "@zxing/library";
+import React, { useEffect, useRef } from "react";
+
+import { BrowserMultiFormatReader } from "@zxing/library";
 
 function App() {
+  const videoRef = useRef(null);
+
+  useEffect(() => {
+    const codeReader = new BrowserMultiFormatReader();
+
+    // Bu, EAN-13 dahil farklı türlerdeki barkodları okuyabilir
+    codeReader
+      .listVideoInputDevices()
+      .then((videoInputDevices) => {
+        const sourceId =
+          videoInputDevices.length > 0
+            ? videoInputDevices[0].deviceId
+            : undefined;
+
+        // EAN-13 formatı için özel bir hint belirtebilirsiniz
+        const hints = new Map();
+        hints.set(BrowserMultiFormatReader.HINT_POSSIBLE_FORMATS, [
+          BarcodeFormat.EAN_13,
+        ]);
+
+        codeReader
+          .decodeFromInputVideoDevice(undefined, videoRef.current, hints)
+          .then((result) => {
+            console.log(`Barkod Okundu: ${result.text}`);
+          })
+          .catch((err) => console.error(err));
+      })
+      .catch((err) => console.error(err));
+
+    // Unmount işlemleri için
+    return () => {
+      codeReader.reset();
+    };
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Barkod Okuyucu</h1>
+      <video ref={videoRef} width={300} height={200}></video>
     </div>
   );
 }
