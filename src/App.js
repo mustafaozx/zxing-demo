790fcfc8c5a835dd162dae186af12432a54f44f6
mustafaozx <mustafaozx@icloud.com>
2023-09-16 18:35:22 +0300

    first

--- src/App.js
+++ src/App.js
@@ -4,11 +4,25 @@ import { BrowserMultiFormatReader } from "@zxing/library";
 
 function App() {
   const videoRef = useRef(null);
-  const [scannedCode, setScannedCode] = useState(null); // Okunan kodu tutacak state
+  const [scannedCode, setScannedCode] = useState(null);
+  const codeReader = new BrowserMultiFormatReader();
+
+  const readCode = () => {
+    codeReader
+      .decodeFromInputVideoDevice(undefined, videoRef.current)
+      .then((result) => {
+        console.log(`Barkod Okundu: ${result.text}`);
+        setScannedCode(result.text);
+        codeReader.reset();
+        setTimeout(readCode, 1000); // 1 saniye bekleyip tekrar okuma işlemi başlatır
+      })
+      .catch((err) => {
+        console.error(err);
+        setTimeout(readCode, 1000); // 1 saniye bekleyip tekrar okuma işlemi başlatır
+      });
+  };
 
   useEffect(() => {
-    const codeReader = new BrowserMultiFormatReader();
-
     codeReader
       .listVideoInputDevices()
       .then((videoInputDevices) => {
@@ -16,18 +30,10 @@ function App() {
           videoInputDevices.length > 0
             ? videoInputDevices[0].deviceId
             : undefined;
-
-        codeReader
-          .decodeFromInputVideoDevice(undefined, videoRef.current)
-          .then((result) => {
-            console.log(`Barkod Okundu: ${result.text}`);
-            setScannedCode(result.text); // Okunan kodu güncelle
-          })
-          .catch((err) => console.error(err));
+        readCode();
       })
       .catch((err) => console.error(err));
 
-    // Unmount işlemleri için
     return () => {
       codeReader.reset();
     };
@@ -37,8 +43,7 @@ function App() {
     <div className="App">
       <h1>Barkod Okuyucu</h1>
       <video ref={videoRef} width={300} height={200}></video>
-      {scannedCode && <p>Okunan Barkod: {scannedCode}</p>}{" "}
-      {/* Eğer barkod okunduysa yazdır */}
+      {scannedCode && <p>Okunan Barkod: {scannedCode}</p>}
     </div>
   );
 }
